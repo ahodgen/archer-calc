93beca9d6d283aea9d9708a18b9a5045a102f441
Aaron Hodgen <ahodgen@kill-9.org>
2015-10-05 23:55:10 -0400

    Shorten names for readability.

--- src/BuiltInMath.hs
+++ src/BuiltInMath.hs
@@ -50,147 +50,146 @@ evenOdd eo x
 intToDbl :: Integer -> Double
 intToDbl = fromIntegral
 
-mathAbs, mathAcos, mathAcosh, mathAsin, mathAsinh, mathAtan, mathAtanh,
-    mathCos, mathCosh, degrees, mathEven, mathExp,
-    mathInt, mathLn, mathLog10, mathOdd, mathRad, mathSign, mathSin, mathSinh,
-    mathTan, mathTanh
+mAbs, mAcos, mAcosh, mAsin, mAsinh, mAtan, mAtanh, mCos, mCosh, mDeg,
+    mEven, mExp, mInt, mLn, mLog10, mOdd, mRad, mSign, mSin, mSinh,
+    mTan, mTanh
      :: [Value] -> Interpreter EvalError Value
 
-mathAbs   = unaryBiHask abs
-mathAcos  = unaryBiHask acos
-mathAcosh = unaryBiHask acosh
-mathAsin  = unaryBiHask asin
-mathAsinh = unaryBiHask asinh
-mathAtan  = unaryBiHask atan
-mathAtanh = unaryBiHask atanh
-mathCos   = unaryBiHask cos
-mathCosh  = unaryBiHask cosh
-degrees   = unaryBiHask (\x -> 180*x/pi)
-mathEven  = unaryBiHask $ evenOdd Even
-mathExp   = unaryBiHask exp
-mathInt   = unaryBiHask (\x -> intToDbl $ truncate (if x < 0 then x-1 else x))
-mathLn    = unaryBiHask log
-mathOdd   = unaryBiHask $ evenOdd Odd
-mathLog10 = unaryBiHask (logBase 10)
-mathRad   = unaryBiHask (\x -> x*pi/180)
-mathSign  = unaryBiHask sign
+mAbs   = unaryBiHask abs
+mAcos  = unaryBiHask acos
+mAcosh = unaryBiHask acosh
+mAsin  = unaryBiHask asin
+mAsinh = unaryBiHask asinh
+mAtan  = unaryBiHask atan
+mAtanh = unaryBiHask atanh
+mCos   = unaryBiHask cos
+mCosh  = unaryBiHask cosh
+mDeg   = unaryBiHask (\x -> 180*x/pi)
+mEven  = unaryBiHask $ evenOdd Even
+mExp   = unaryBiHask exp
+mInt   = unaryBiHask (\x -> intToDbl $ truncate (if x < 0 then x-1 else x))
+mLn    = unaryBiHask log
+mOdd   = unaryBiHask $ evenOdd Odd
+mLog10 = unaryBiHask (logBase 10)
+mRad   = unaryBiHask (\x -> x*pi/180)
+mSign  = unaryBiHask sign
   where
     sign x | x > 0     = 1
            | x < 0     = -1
            | otherwise = 0
-mathSin   = unaryBiHask sin
-mathSinh  = unaryBiHask sinh
-mathTan   = unaryBiHask tan
-mathTanh  = unaryBiHask tanh
+mSin   = unaryBiHask sin
+mSinh  = unaryBiHask sinh
+mTan   = unaryBiHask tan
+mTanh  = unaryBiHask tanh
 
-mathAtan2 :: [Value] -> Interpreter EvalError Value
-mathAtan2 [VNum x, VNum y] = return . VNum $ atan2 y x
-mathAtan2 _ = infError
+mAtan2 :: [Value] -> Interpreter EvalError Value
+mAtan2 [VNum x, VNum y] = return . VNum $ atan2 y x
+mAtan2 _ = infError
 
-mathCeil :: [Value] -> Interpreter EvalError Value
-mathCeil [VNum x, VNum sig] = return . VNum $ sig * intToDbl (ceiling (x/sig))
-mathCeil _ = infError
+mCeil :: [Value] -> Interpreter EvalError Value
+mCeil [VNum x, VNum sig] = return . VNum $ sig * intToDbl (ceiling (x/sig))
+mCeil _ = infError
 
-mathComb :: [Value] -> Interpreter EvalError Value
-mathComb [VNum n, VNum r] = return $ VNum x
+mComb :: [Value] -> Interpreter EvalError Value
+mComb [VNum n, VNum r] = return $ VNum x
   where
     x = intToDbl (fact n) / intToDbl (fact r * fact (n-r))
     fact y = product [1..floor y]
-mathComb _ = infError
+mComb _ = infError
 
-mathFact :: [Value] -> Interpreter EvalError Value
-mathFact [VNum n] | n >= 0    = return . VNum . intToDbl $ product [1..floor n]
+mFact :: [Value] -> Interpreter EvalError Value
+mFact [VNum n] | n >= 0    = return . VNum . intToDbl $ product [1..floor n]
                   | otherwise = throwError $ EvBuiltInError "Factorial on negative"
-mathFact _ = infError
+mFact _ = infError
 
-mathFloor :: [Value] -> Interpreter EvalError Value
-mathFloor [VNum x, VNum sig] = return . VNum $ sig * intToDbl (floor (x/sig))
-mathFloor _ = infError
+mFloor :: [Value] -> Interpreter EvalError Value
+mFloor [VNum x, VNum sig] = return . VNum $ sig * intToDbl (floor (x/sig))
+mFloor _ = infError
 
-mathLog :: [Value] -> Interpreter EvalError Value
-mathLog [VNum x, VNum b] = return . VNum $ logBase b x
-mathLog _ = infError
+mLog :: [Value] -> Interpreter EvalError Value
+mLog [VNum x, VNum b] = return . VNum $ logBase b x
+mLog _ = infError
 
-mathMod :: [Value] -> Interpreter EvalError Value
-mathMod [VNum n, VNum d] = return . VNum . intToDbl $ mod (floor n ) (floor d)
-mathMod _ = infError
+mMod :: [Value] -> Interpreter EvalError Value
+mMod [VNum n, VNum d] = return . VNum . intToDbl $ mod (floor n ) (floor d)
+mMod _ = infError
 
-mathPi :: [Value] -> Interpreter EvalError Value
-mathPi [] = return . VNum $ pi
-mathPi _ = infError
+mPi :: [Value] -> Interpreter EvalError Value
+mPi [] = return . VNum $ pi
+mPi _ = infError
 
-mathQuot :: [Value] -> Interpreter EvalError Value
-mathQuot [VNum n, VNum d] = return . VNum . intToDbl $ floor n `quot` floor d
-mathQuot _ = infError
+mQuot :: [Value] -> Interpreter EvalError Value
+mQuot [VNum n, VNum d] = return . VNum . intToDbl $ floor n `quot` floor d
+mQuot _ = infError
 
-mathRound :: [Value] -> Interpreter EvalError Value
-mathRound [VNum n, VNum d] = weirdRound round round n d
-mathRound _ = infError
+mRound :: [Value] -> Interpreter EvalError Value
+mRound [VNum n, VNum d] = weirdRound round round n d
+mRound _ = infError
 
-mathRDown :: [Value] -> Interpreter EvalError Value
-mathRDown [VNum n, VNum d] = weirdRound ceiling floor n d
-mathRDown _ = infError
+mRDown :: [Value] -> Interpreter EvalError Value
+mRDown [VNum n, VNum d] = weirdRound ceiling floor n d
+mRDown _ = infError
 
-mathRndUp :: [Value] -> Interpreter EvalError Value
-mathRndUp [VNum n, VNum d] = weirdRound floor ceiling n d
-mathRndUp _ = infError
+mRndUp :: [Value] -> Interpreter EvalError Value
+mRndUp [VNum n, VNum d] = weirdRound floor ceiling n d
+mRndUp _ = infError
 
-mathSqrt :: [Value] -> Interpreter EvalError Value
-mathSqrt [VNum x] = if x < 0
+mSqrt :: [Value] -> Interpreter EvalError Value
+mSqrt [VNum x] = if x < 0
     then throwError $ EvBuiltInError "Cannot take sqrt of a negative."
     else return . VNum $ sqrt x
-mathSqrt _ = infError
+mSqrt _ = infError
 
-mathSum :: [Value] -> Interpreter EvalError Value
-mathSum [VNum x, VNum y] = return . VNum $ x + y
-mathSum _ = infError
+mSum :: [Value] -> Interpreter EvalError Value
+mSum [VNum x, VNum y] = return . VNum $ x + y
+mSum _ = infError
 
 mSumSq :: [Value] -> Interpreter EvalError Value
 mSumSq [VNum a,VNum b] = return . VNum $ a ** 2 + b ** 2
 mSumSq _ = infError
 
-mathTrunc :: [Value] -> Interpreter EvalError Value
-mathTrunc [VNum n, VNum d] = weirdRound round round n d
-mathTrunc _ = infError
+mTrunc :: [Value] -> Interpreter EvalError Value
+mTrunc [VNum n, VNum d] = weirdRound round round n d
+mTrunc _ = infError
 
 builtInMath :: M.Map String BuiltIn
 builtInMath = M.fromList
-    [ ("abs",     BuiltIn mathAbs   "ABS"    (typeNum :-> typeNum))
-    , ("acos",    BuiltIn mathAcos  "ACOS"   (typeNum :-> typeNum))
-    , ("acosh",   BuiltIn mathAcosh "ACOSH"  (typeNum :-> typeNum))
-    , ("asin",    BuiltIn mathAsin  "ASIN"   (typeNum :-> typeNum))
-    , ("asinh",   BuiltIn mathAsinh "ASINH"  (typeNum :-> typeNum))
-    , ("atan",    BuiltIn mathAtan  "ATAN"   (typeNum :-> typeNum))
-    , ("atan2",   BuiltIn mathAtan2 "ATAN2"  (typeNum :-> typeNum :-> typeNum))
-    , ("atanh",   BuiltIn mathAtanh "ATANH"  (typeNum :-> typeNum))
-    , ("ceiling", BuiltIn mathCeil  "CEILING" (typeNum :-> typeNum :-> typeNum))
-    , ("combin",  BuiltIn mathComb  "COMBIN" (typeNum :-> typeNum :-> typeNum))
-    , ("cos",     BuiltIn mathCos   "COS"    (typeNum :-> typeNum))
-    , ("cosh",    BuiltIn mathCosh  "COSH"   (typeNum :-> typeNum))
-    , ("degrees", BuiltIn degrees   "DEGREES" (typeNum :-> typeNum))
-    , ("even",    BuiltIn mathEven  "EVEN"   (typeNum :-> typeNum))
-    , ("exp",     BuiltIn mathExp   "EXP"    (typeNum :-> typeNum))
-    , ("fact",    BuiltIn mathFact  "FACT"   (typeNum :-> typeNum))
-    , ("floor",   BuiltIn mathFloor "FLOOR"  (typeNum :-> typeNum :-> typeNum))
-    , ("int",     BuiltIn mathInt   "INT"    (typeNum :-> typeNum))
-    , ("ln",      BuiltIn mathLn    "LN"     (typeNum :-> typeNum))
-    , ("log",     BuiltIn mathLog   "LOG"    (typeNum :-> typeNum :-> typeNum))
-    , ("log10",   BuiltIn mathLog10 "LOG10"  (typeNum :-> typeNum))
-    , ("mod",     BuiltIn mathMod   "MOD"    (typeNum :-> typeNum :-> typeNum))
-    , ("odd",     BuiltIn mathOdd   "ODD"    (typeNum :-> typeNum))
-    , ("pi",      BuiltIn mathPi    "PI"      typeNum)
-    , ("quot",    BuiltIn mathQuot  "QUOTIENT" (typeNum :-> typeNum :-> typeNum))
-    , ("radians", BuiltIn mathRad   "RADIANS" (typeNum :-> typeNum))
-    , ("round",   BuiltIn mathRound "ROUND"  (typeNum :-> typeNum :-> typeNum))
-    , ("rounddown",BuiltIn mathRDown "ROUNDDOWN" (typeNum :-> typeNum :-> typeNum))
-    , ("roundup", BuiltIn mathRndUp "ROUNDUP" (typeNum :-> typeNum :-> typeNum))
-    , ("sum",     BuiltIn mathSum   "SUM"    (typeNum :-> typeNum :-> typeNum))
-    , ("sign",    BuiltIn mathSign  "SIGN"   (typeNum :-> typeNum))
-    , ("sin",     BuiltIn mathSin   "SIN"    (typeNum :-> typeNum))
-    , ("sinh",    BuiltIn mathSinh  "SINH"   (typeNum :-> typeNum))
-    , ("sqrt",    BuiltIn mathSqrt  "SQRT"   (typeNum :-> typeNum))
-    , ("sumsq",   BuiltIn mSumSq    "SUMSQ"  (typeNum :-> typeNum :-> typeNum))
-    , ("tan",     BuiltIn mathTan   "TAN"    (typeNum :-> typeNum))
-    , ("tanh",    BuiltIn mathTanh  "TANH"   (typeNum :-> typeNum))
-    , ("trunc",   BuiltIn mathTrunc "TRUNC"  (typeNum :-> typeNum :-> typeNum))
+    [ ("abs",     BuiltIn mAbs   "ABS"    (typeNum :-> typeNum))
+    , ("acos",    BuiltIn mAcos  "ACOS"   (typeNum :-> typeNum))
+    , ("acosh",   BuiltIn mAcosh "ACOSH"  (typeNum :-> typeNum))
+    , ("asin",    BuiltIn mAsin  "ASIN"   (typeNum :-> typeNum))
+    , ("asinh",   BuiltIn mAsinh "ASINH"  (typeNum :-> typeNum))
+    , ("atan",    BuiltIn mAtan  "ATAN"   (typeNum :-> typeNum))
+    , ("atan2",   BuiltIn mAtan2 "ATAN2"  (typeNum :-> typeNum :-> typeNum))
+    , ("atanh",   BuiltIn mAtanh "ATANH"  (typeNum :-> typeNum))
+    , ("ceiling", BuiltIn mCeil  "CEILING" (typeNum :-> typeNum :-> typeNum))
+    , ("combin",  BuiltIn mComb  "COMBIN" (typeNum :-> typeNum :-> typeNum))
+    , ("cos",     BuiltIn mCos   "COS"    (typeNum :-> typeNum))
+    , ("cosh",    BuiltIn mCosh  "COSH"   (typeNum :-> typeNum))
+    , ("degrees", BuiltIn mDeg   "DEGREES" (typeNum :-> typeNum))
+    , ("even",    BuiltIn mEven  "EVEN"   (typeNum :-> typeNum))
+    , ("exp",     BuiltIn mExp   "EXP"    (typeNum :-> typeNum))
+    , ("fact",    BuiltIn mFact  "FACT"   (typeNum :-> typeNum))
+    , ("floor",   BuiltIn mFloor "FLOOR"  (typeNum :-> typeNum :-> typeNum))
+    , ("int",     BuiltIn mInt   "INT"    (typeNum :-> typeNum))
+    , ("ln",      BuiltIn mLn    "LN"     (typeNum :-> typeNum))
+    , ("log",     BuiltIn mLog   "LOG"    (typeNum :-> typeNum :-> typeNum))
+    , ("log10",   BuiltIn mLog10 "LOG10"  (typeNum :-> typeNum))
+    , ("mod",     BuiltIn mMod   "MOD"    (typeNum :-> typeNum :-> typeNum))
+    , ("odd",     BuiltIn mOdd   "ODD"    (typeNum :-> typeNum))
+    , ("pi",      BuiltIn mPi    "PI"      typeNum)
+    , ("quot",    BuiltIn mQuot "QUOTIENT" (typeNum :-> typeNum :-> typeNum))
+    , ("radians", BuiltIn mRad   "RADIANS"(typeNum :-> typeNum))
+    , ("round",   BuiltIn mRound "ROUND" (typeNum :-> typeNum :-> typeNum))
+    , ("rounddown",BuiltIn mRDown "ROUNDDOWN" (typeNum :-> typeNum :-> typeNum))
+    , ("roundup", BuiltIn mRndUp "ROUNDUP" (typeNum :-> typeNum :-> typeNum))
+    , ("sum",     BuiltIn mSum   "SUM"   (typeNum :-> typeNum :-> typeNum))
+    , ("sign",    BuiltIn mSign  "SIGN"  (typeNum :-> typeNum))
+    , ("sin",     BuiltIn mSin   "SIN"   (typeNum :-> typeNum))
+    , ("sinh",    BuiltIn mSinh  "SINH"  (typeNum :-> typeNum))
+    , ("sqrt",    BuiltIn mSqrt  "SQRT"  (typeNum :-> typeNum))
+    , ("sumsq",   BuiltIn mSumSq "SUMSQ" (typeNum :-> typeNum :-> typeNum))
+    , ("tan",     BuiltIn mTan   "TAN"   (typeNum :-> typeNum))
+    , ("tanh",    BuiltIn mTanh  "TANH"  (typeNum :-> typeNum))
+    , ("trunc",   BuiltIn mTrunc "TRUNC" (typeNum :-> typeNum :-> typeNum))
     ]
